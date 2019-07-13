4260bd4d95225cf9d20707b0bb592a0e140bc6d9
emmanuelcodev <emmanuel.adw@gmail.com>
2019-07-13 14:52:07 -0700

    added interview question for array

--- extra_interview_questions/LinkedLists/Q1_emmanuelcodev/node.py
+++
@@ -1,6 +0,0 @@
-class Node(object):
-    def __init__(self, data):
-        if data is None:
-            raise Exception('data cannot be of NoneType')
-        self.data = data
-        self.next = None
