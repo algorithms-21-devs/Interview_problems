ec3d961ba57072ff079596a246d81859e8cc78d8
Marco Aguilar <aguilaramarco@yahoo.com>
2019-07-19 22:12:34 -0700

    Finished Q5.

---
+++ weekly_interview_questions/IQ_5/Q5_maguil53/Q5Medium_O(logn).py
@@ -0,0 +1,27 @@
+# Time Complexity: O(logn)
+def reverseIntArray(int_list):
+    if int_list is None:
+        raise Exception("Can't enter None")
+
+    length = len(int_list)
+
+    if length == 0:
+        return None
+
+    half = length // 2
+
+    for i in range(half):
+        temp = int_list[i]
+        # Swap element on left with one on the right
+        int_list[i] = int_list[length - 1 - i]
+        # Swap element on right with one on the left
+        int_list[length - 1 - i] = temp
+
+    return int_list
+
+
+print(reverseIntArray([5, 6, 9, 2]))
+print(reverseIntArray([-3, 4]))
+print(reverseIntArray([0]))
+print(reverseIntArray([]))
+print(reverseIntArray(None))
